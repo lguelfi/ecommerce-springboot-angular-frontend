fdacb83436b0bf5ecc1ba82c7d014bce0d8891da
Leonardo Guelfi <56708262+lguelfi@users.noreply.github.com>
2023-10-02 20:01:51 -0300

    Update my-app-config.ts

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
-        clientId: '0oaatjcgj6exfU3Mw5d7',
-        issuer: 'https://dev-37078048.okta.com/oauth2/default',
+        clientId: '',
+        issuer: '',
         redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
